e8e55153245a477da4d16b12bb9107bdc4e60f39
101283501 <59575737+101283501@users.noreply.github.com>
2020-01-21 11:01:52 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-console.log('It works!')
-var vNumber = 3
-let lString = 'This is a string'
-const cBoolean = true
-console.log(vNumber, lString. cBoolean)
-if (typeof vNumber = 'number') { console.log('This var variable is the number' vNumber) }
-if (typeof lString = 'string') { console.log('This let variable is the string' : lString) }
-if (typeof cBoolean = 'boolean') { console.log('This const variable is true') } 
